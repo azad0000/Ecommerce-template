f0b871df587d64225f15c0b5d54e31c257f97a88
azad0000 <mhazad0000@gmail.com>
2021-09-26 01:26:29 +0600

    Upto Offer section

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.main_hero').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
